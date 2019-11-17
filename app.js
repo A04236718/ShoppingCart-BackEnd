36351612062cc05ded0e14e5195ba6184251428b
Muhammad Ahmed <muhammadahmed@Muhammads-MacBook-Pro-2.local>
2019-11-17 15:21:03 -0600

    Mongo collection created, Connected to db, Env created

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const logger = require("morgan");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+// Module to hide our Mongo Credentials
+require('dotenv/config');
+
+
 
 const app = express();
 const port = process.env.PORT || 3000; // Defaults to port 3000
@@ -11,7 +16,7 @@ const items = require('./routes/items');
 
 app.use(logger("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Component Route Endpoints
 app.use("/", index);
@@ -25,10 +30,15 @@ app.use((req, res, next) => {
 })
 
 app.use((req, res, next) => {
-    res.status(err.status || 500).json({message:err.message});
+    res.status(err.status || 500).json({ message: err.message });
 });
 
 // Event Listener 
 app.listen(port);
 console.log(`server is running on ${port}`);
 
+// Connect to the DB
+mongoose.connect(process.env.DB_CONNECTION, 
+{useNewUrlParser: true},
+() => console.log("Connected to DB Successfully!")
+); 
